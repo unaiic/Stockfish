0f033efc31fe338932d882ad68ea105b17f09975
Unai Corzo <corzounai@gmail.com>
2021-01-01 10:44:40 +0100

    tweak

--- src/evaluate.cpp
+++ src/evaluate.cpp
@@ -190,10 +190,8 @@ namespace {
   constexpr Value LazyThreshold1 =  Value(1565);
   constexpr Value LazyThreshold2 =  Value(1102);
   constexpr Value SpaceThreshold = Value(11551);
-  Value NNUEThreshold1 =   Value(682);
-  Value NNUEThreshold2 =   Value(176);
-  int scaling = 679;
-  TUNE(NNUEThreshold1, NNUEThreshold2, scaling);
+  constexpr Value NNUEThreshold1 =   Value(682);
+  constexpr Value NNUEThreshold2 =   Value(176);
 
   // KingAttackWeights[PieceType] contains king attack weights by piece type
   constexpr int KingAttackWeights[PIECE_TYPE_NB] = { 0, 0, 81, 52, 44, 10 };
@@ -1055,7 +1053,7 @@ Value Eval::evaluate(const Position& pos) {
       // Scale and shift NNUE for compatibility with search and classical evaluation
       auto  adjusted_NNUE = [&](){
          int mat = pos.non_pawn_material() + PawnValueMg * pos.count<PAWN>();
-         return NNUE::evaluate(pos) * (scaling + mat / 32) / 1024 + Tempo;
+         return NNUE::evaluate(pos) * (679 + mat / 32) / 1024 + Tempo;
       };
 
       // If there is PSQ imbalance use classical eval, with small probability if it is small
