4a5d2ee33cee97968c183a65aa07a6e7d60395e7
Unai Corzo <corzounai@gmail.com>
2021-01-30 20:37:35 +0100

    tweak

--- src/search.cpp
+++ src/search.cpp
@@ -1135,6 +1135,17 @@ moves_loop: // When in check, search starts from here
                && pos.non_pawn_material() <= 2 * RookValueMg)
           extension = 1;
 
+      // Castling extension
+      if (type_of(move) == CASTLING)
+      {
+          if (popcount(pos.pieces(us) & ~pos.pieces(PAWN) & (to_sq(move) & KingSide ? KingSide : QueenSide)) <= 2)
+              extension = 1;
+          else if (   !pos.can_castle(~us & ANY_CASTLING)
+                   && (to_sq(move) & (pos.pieces(~us, KING) & KingSide ? KingSide : QueenSide))
+                   && (popcount(pos.pieces(~us) & ~pos.pieces(PAWN) & (to_sq(move) & KingSide ? KingSide : QueenSide)) <= 4))
+              extension = 1;
+      }
+
       // Add extension to new depth
       newDepth += extension;
 
