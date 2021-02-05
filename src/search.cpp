d5d99d9cf3375d4b6df098e3d6fd839c839fce78
Unai Corzo <corzounai@gmail.com>
2021-02-05 17:27:41 +0100

    tweak

--- src/search.cpp
+++ src/search.cpp
@@ -1198,6 +1198,9 @@ moves_loop: // When in check, search starts from here
               if (   !givesCheck
                   && ss->staticEval + PieceValue[EG][pos.captured_piece()] + 210 * depth <= alpha)
                   r++;
+
+              if (ss->excludedMove && !ttCapture)
+                r--;
           }
           else
           {
