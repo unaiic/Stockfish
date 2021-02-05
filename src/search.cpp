4d27e7f05ed613cd43b647b251e0796810942bae
Unai Corzo <corzounai@gmail.com>
2021-02-05 17:38:39 +0100

    tweak

--- src/search.cpp
+++ src/search.cpp
@@ -714,6 +714,11 @@ namespace {
                 int penalty = -stat_bonus(depth);
                 thisThread->mainHistory[us][from_to(ttMove)] << penalty;
                 update_continuation_histories(ss, pos.moved_piece(ttMove), to_sq(ttMove), penalty);
+                if (ttMove == ss->killers[0] && ss->killers[1])
+                {
+                    ss->killers[0] = ss->killers[1];
+                    ss->killers[1] = ttMove;
+                }
             }
         }
 
