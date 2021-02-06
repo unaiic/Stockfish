0765fbf53a67ecfc1fd46153ba62a37c852f7e7d
Unai Corzo <corzounai@gmail.com>
2021-02-06 16:43:26 +0100

    tweak

--- src/search.cpp
+++ src/search.cpp
@@ -992,7 +992,7 @@ moves_loop: // When in check, search starts from here
 
     // Step 11. Loop through all pseudo-legal moves until no moves remain
     // or a beta cutoff occurs.
-    while ((move = mp.next_move(moveCountPruning)) != MOVE_NONE)
+    while ((move = mp.next_move(moveCountPruning && !PvNode)) != MOVE_NONE)
     {
       assert(is_ok(move));
 
