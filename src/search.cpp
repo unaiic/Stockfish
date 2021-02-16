6c488bd6c32c60397d123dac0dfd8ab8d3070f3b
Unai Corzo <corzounai@gmail.com>
2021-02-16 17:07:39 +0100

    tweak

--- src/search.cpp
+++ src/search.cpp
@@ -1712,7 +1712,7 @@ moves_loop: // When in check, search starts from here
   void update_all_stats(const Position& pos, Stack* ss, Move bestMove, Value bestValue, Value beta, Square prevSq,
                         Move* quietsSearched, int quietCount, Move* capturesSearched, int captureCount, Depth depth) {
 
-    int bonus1, bonus2;
+    int bonus1, bonus2, bonus3;
     Color us = pos.side_to_move();
     Thread* thisThread = pos.this_thread();
     CapturePieceToHistory& captureHistory = thisThread->captureHistory;
@@ -1726,7 +1726,8 @@ moves_loop: // When in check, search starts from here
     if (!pos.capture_or_promotion(bestMove))
     {
         // Increase stats for the best move in case it was a quiet move
-        update_quiet_stats(pos, ss, bestMove, bonus2, depth);
+        bonus3 = bonus2 * (2 * quietCount + 16) / (quietCount + 16);
+        update_quiet_stats(pos, ss, bestMove, bonus3, depth);
 
         // Decrease stats for all non-best quiet moves
         for (int i = 0; i < quietCount; ++i)
