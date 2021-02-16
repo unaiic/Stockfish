156182ea7c17965ab2a018da4f6a58291fc7bcac
Unai Corzo <corzounai@gmail.com>
2021-02-16 17:08:52 +0100

    tweak

--- src/search.cpp
+++ src/search.cpp
@@ -1726,7 +1726,7 @@ moves_loop: // When in check, search starts from here
     if (!pos.capture_or_promotion(bestMove))
     {
         // Increase stats for the best move in case it was a quiet move
-        bonus3 = bonus2 * (2 * quietCount + 16) / (quietCount + 16);
+        bonus3 = bonus2 * (2 * quietCount + 8) / (quietCount + 8);
         update_quiet_stats(pos, ss, bestMove, bonus3, depth);
 
         // Decrease stats for all non-best quiet moves
