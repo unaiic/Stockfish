e7e67e7d61f927f557648f758f76e6e3808b9400
Unai Corzo <corzounai@gmail.com>
2021-02-20 13:53:03 +0100

    tweak

--- src/search.cpp
+++ src/search.cpp
@@ -57,12 +57,9 @@ using namespace Search;
 
 namespace {
 
-  int netbiases[1] = {-193};
-  TUNE(netbiases);
-  int netweights[32] = {-27,  -16,  -76,  57, -21,  121,  -118, 25, 31, 52, -34,  22, 13, -37,  -20,  96, 
-  -57,  34, 36, 41, -18,  -19,  16, -31,  -12,  -36,  -22,  -10,  -33,  26, -12,  18};
-  auto myfunc127 = [](int m){ return std::pair<int, int>(std::max(-127, m - 80),std::min(127,m + 80));};
-  TUNE(SetRange(myfunc127), netweights);
+  int netbiases[1] = {-173};
+  int netweights[32] = {-29,  -16,  -92,  53, -35,  121,  -122, 41, 49, 64, -40,  22, 17, -25,  -30,  110, 
+  -71,  38, 42, 35, -28,  -35,  12, -41,  -34,  -62,  -18,  -22,  -39,  28, -10,  26};
 
   // Different node types, used as a template parameter
   enum NodeType { NonPV, PV };
