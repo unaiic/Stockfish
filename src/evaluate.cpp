6ea27e40bea1ef971bb1471d3a8f08a7cdc0d426
Unai Corzo <corzounai@gmail.com>
2021-02-21 09:34:56 +0100

    tweak

--- src/evaluate.cpp
+++ src/evaluate.cpp
@@ -1054,9 +1054,6 @@ Value Eval::evaluate(const Position& pos) {
       v = NNUE::evaluate(pos) * (641 + mat / 32 - 4 * pos.rule50_count()) / 1024 + Tempo;
   }
 
-  // Damp down the evaluation linearly when shuffling
-  v = v * (100 - pos.rule50_count()) / 100;
-
   // Guarantee evaluation does not hit the tablebase range
   v = std::clamp(v, VALUE_TB_LOSS_IN_MAX_PLY + 1, VALUE_TB_WIN_IN_MAX_PLY - 1);
 
