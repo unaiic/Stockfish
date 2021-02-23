2ca2daef6712b55309dd924da03b79c320a74ed1
Unai Corzo <corzounai@gmail.com>
2021-02-23 17:56:23 +0100

    tweak

--- src/evaluate.cpp
+++ src/evaluate.cpp
@@ -191,10 +191,10 @@ namespace {
   constexpr Value LazyThreshold1 =  Value(1565);
   constexpr Value LazyThreshold2 =  Value(1102);
   constexpr Value SpaceThreshold = Value(11551);
-  constexpr Value NNUEThreshold1 =   Value(772);
+  constexpr Value NNUEThreshold1 =   Value(790);
   constexpr Value NNUEThreshold2 =   Value(177);
 
-  int scaling = 500;
+  int scaling = 496;
 
   // KingAttackWeights[PieceType] contains king attack weights by piece type
   constexpr int KingAttackWeights[PIECE_TYPE_NB] = { 0, 0, 81, 52, 44, 10 };
