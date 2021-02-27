4693cbdf5a9d5f8658bab32b6ec96ab030ae0f48
Unai Corzo <corzounai@gmail.com>
2021-02-27 13:42:24 +0100

    tweak

--- src/evaluate.cpp
+++ src/evaluate.cpp
@@ -1060,7 +1060,8 @@ Value Eval::evaluate(const Position& pos) {
       Value psq = Value(abs(eg_value(pos.psq_score())));
       int   r50 = 16 + pos.rule50_count();
       bool  largePsq = psq * 16 > (NNUEThreshold1 + pos.non_pawn_material() / 64) * r50;
-      bool  classical = largePsq || (psq > PawnValueMg / 4 && !(pos.this_thread()->nodes & 0xB));
+      bool  classical =   largePsq
+                       || (!(pos.this_thread()->nodes & 0xB) && psq > PawnValueMg / 4 && pos.non_pawn_material() < 16200);
 
       // Use classical evaluation for really low piece endgames.
       // The most critical case is a bishop + A/H file pawn vs naked king draw.
