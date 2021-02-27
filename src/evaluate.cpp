6fddb6208c00f6e3041f3a4af8842e47ca5d2946
Unai Corzo <corzounai@gmail.com>
2021-02-27 13:58:21 +0100

    tweak

--- src/evaluate.cpp
+++ src/evaluate.cpp
@@ -1060,18 +1060,27 @@ Value Eval::evaluate(const Position& pos) {
       Value psq = Value(abs(eg_value(pos.psq_score())));
       int   r50 = 16 + pos.rule50_count();
       bool  largePsq = psq * 16 > (NNUEThreshold1 + pos.non_pawn_material() / 64) * r50;
-      bool  classical = largePsq || (psq > PawnValueMg / 4 && !(pos.this_thread()->nodes & 0xB));
+      bool  classical =   largePsq
+                       || (!(pos.this_thread()->nodes & 0xB) && psq > PawnValueMg / 4);
 
       // Use classical evaluation for really low piece endgames.
       // The most critical case is a bishop + A/H file pawn vs naked king draw.
-      bool strongClassical = pos.non_pawn_material() < 2 * RookValueMg && pos.count<PAWN>() < 2;
+      bool strongClassical =   pos.non_pawn_material() < 2 * RookValueMg
+                            && pos.count<PAWN>() < 2;
 
-      v = classical || strongClassical ? Evaluation<NO_TRACE>(pos).value() : adjusted_NNUE();
+      // Use NNUE for bishop endgames with moderate number of pawns
+      bool strongNNUE =   pos.non_pawn_material() == BishopValueMg
+                       && pos.count<PAWN>() <= 8
+                       && pos.count<PAWN>() >= 4;
+
+      v = (classical || strongClassical) && !strongNNUE ? Evaluation<NO_TRACE>(pos).value() : adjusted_NNUE();
 
       // If the classical eval is small and imbalance large, use NNUE nevertheless.
       // For the case of opposite colored bishops, switch to NNUE eval with
       // small probability if the classical eval is less than the threshold.
-      if (   largePsq && !strongClassical
+      if (   largePsq
+          && !strongClassical
+          && !strongNNUE
           && (   abs(v) * 16 < NNUEThreshold2 * r50
               || (   pos.opposite_bishops()
                   && abs(v) * 16 < (NNUEThreshold1 + pos.non_pawn_material() / 64) * r50
