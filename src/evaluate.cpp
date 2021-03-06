2169b727d72b96563800bfbbaab27a380dfee9e9
Unai Corzo <corzounai@gmail.com>
2021-03-06 13:56:46 +0100

    tweak

--- src/evaluate.cpp
+++ src/evaluate.cpp
@@ -191,10 +191,10 @@ namespace {
   constexpr Value LazyThreshold1 =  Value(1565);
   constexpr Value LazyThreshold2 =  Value(1102);
   constexpr Value SpaceThreshold = Value(11551);
-  Value NNUEThreshold1 =   Value(682);
-  Value NNUEThreshold2 =   Value(176);
+  constexpr Value NNUEThreshold1 =   Value(882);
+  constexpr Value NNUEThreshold2 =   Value(124);
 
-  int scaling = 641;
+  constexpr int scaling = 667;
 
   TUNE(NNUEThreshold1, NNUEThreshold2, scaling);
 
