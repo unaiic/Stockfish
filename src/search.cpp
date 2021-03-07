6cbd4d929e6fd7dd8537fd329f7e37c9a871394a
Unai Corzo <corzounai@gmail.com>
2021-03-07 13:57:45 +0100

    tweak

--- src/search.cpp
+++ src/search.cpp
@@ -1203,8 +1203,8 @@ moves_loop: // When in check, search starts from here
 
           // Decrease reduction if position is or has been on the PV
           // and node is not likely to fail low. (~10 Elo)
-          if (ss->ttPv && !likelyFailLow)
-              r -= 2;
+          if (ss->ttPv)
+              r -= 1 + !likelyFailLow;
 
           // Increase reduction at root and non-PV nodes when the best move does not change frequently
           if ((rootNode || !PvNode) && thisThread->rootDepth > 10 && thisThread->bestMoveChanges <= 2)
