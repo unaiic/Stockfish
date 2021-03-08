720093d95b9c9b6d485265bee0f380b8c701ab90
Unai Corzo <corzounai@gmail.com>
2021-03-08 19:20:07 +0100

    tweak

--- src/search.cpp
+++ src/search.cpp
@@ -57,11 +57,10 @@ using namespace Search;
 
 namespace {
 
-  int netbiases[1] = {-193};
-  TUNE(netbiases);
-  int netweights[32] = {-27,  -16,  -76,  57, -21,  121,  -118, 25, 31, 52, -34,  22, 13, -37,  -20,  96, 
-  -57,  34, 36, 41, -18,  -19,  16, -31,  -12,  -36,  -22,  -10,  -33,  26, -12,  18};
-  TUNE(netweights);
+  constexpr int netbiases[1] = {-241};
+
+  constexpr int netweights[32] = {-29,  -10,  -102,  59, -19,  117,  -120, 28, 23, 48, -21,  26, 11, -15,  -17,  100, 
+  -55,  26, 54, 45, -19,  -27,  16, -28,  -14,  -39,  -21,  -7,  -37,  28, -16,  16};
 
   // Different node types, used as a template parameter
   enum NodeType { NonPV, PV };
